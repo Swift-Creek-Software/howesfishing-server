6dbdb99f40503df5a465c8d21d20434793be029a
root <root@nodejs-howes-fishing.localdomain>
2017-05-20 00:24:03 +0000

    Revert "feat(): finish cron job for 3 day fishing trip reminder"
    This reverts commit 202de5a345493c6f98d19700b2f0f9886e4c3532.

--- api-server/api/services/DateService.js
+++
@@ -1,5 +0,0 @@
-const moment = require('moment-timezone')
-
-module.exports = {
-	formatEmailDate: (date) => moment(date).tz('America/Denver').format('MMMM DD, YYYY'),
-}
--- api-server/config/cron.js
+++ api-server/config/cron.js
@@ -1,51 +1,10 @@
-
 // https://github.com/ghaiklor/sails-hook-cron
 module.exports.cron = {
   sendReminderEmail: {
     // run at 9am everyday
     schedule: '* 9 * * * *',
     onTick: function () {
-      console.log('++ Cron job running: sendReminderEmail')
-
-	    const threeDays = (3 * 24 * 60 * 60 * 1000)
-	    const startDate = new Date(new Date(new Date().getTime() + threeDays).setHours(0,0,0,0))
-	    const endDate = new Date(new Date(startDate).setHours(23,59,59,999))
-
-	    Trip
-		    .find({ where: { startTime: { '>=': startDate, '<=': endDate }, deleted: false } })
-		    .then(trips => {
-		    	console.log(`++ Trips found: `,trips.length)
-
-			    return Promise.all(trips.map(trip => {
-					    return EmailService.sendEmail({
-						    recipients: [
-							    {
-								    address: trip.email,
-							    },
-							    {
-								    address: {
-									    'email': 'reservations@howesfishing.com',
-									    'header_to': trip.email,
-								    },
-							    },
-						    ],
-						    templateId: 'howes-fishing',
-						    templateData: {
-							    firstName: trip.firstName,
-							    confirm: 'remind',
-							    directions: trip.directions,
-							    from: trip.userName,
-							    subject: `Trip fishing reminder ${DateService.formatEmailDate(trip.startTime)}`,
-							    timeCost: trip.clientEmailTemplate,
-						    },
-						    campaignId: 'Client reminder',
-					    })
-			    }))
-		    })
-		    .catch(err => {
-		    	console.error(`++ Error in cron sendReminderEmail`,JSON.stringify(err))
-		    })
-    },
-		start: true,
-  },
+      // console.log('Cron job ran...')
+    }
+  }
 }
