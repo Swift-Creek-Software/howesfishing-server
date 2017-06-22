3b5a2b123b5512ade628d9cffbb7850473a781a0
Andrew Gross <grossmail1@gmail.com>
2017-06-22 17:01:18 -0600

    test new db

--- api-server/config/connections.js
+++ api-server/config/connections.js
@@ -1,27 +1,27 @@
 module.exports.connections = {
 
-  localDiskDb: {
-    adapter: 'sails-disk'
-  },
+	localDiskDb: {
+		adapter: 'sails-disk'
+	},
 
-  // prod DB
-  mongoApiV1ServerProd: {
-    adapter: 'sails-mongo',
-    host: 'ds133158.mlab.com',
-    port: 33158,
-    user: 'db_user',
-    password: '99Fish/99',
-    database: 'aablefishing-api-v1'
-  },
+	// prod DB
+	mongoApiV1ServerProd: {
+		adapter: 'sails-mongo',
+		host: 'cluster0-shard-00-00-nsd2f.mongodb.net',
+		port: 27017,
+		user: 'db_user',
+		password: '9etTRUP3ifFCO2Jc',
+		database: 'aablefishing-api-v1-dev'
+	},
 
-  // dev DB
-  mongoApiV1ServerDev: {
-    adapter: 'sails-mongo',
-    host: 'ds133158.mlab.com',
-    port: 33158,
-    user: 'db_user',
-    password: '99Fish/99',
-    database: 'aablefishing-api-v1-dev'
-  },
+	// dev DB
+	mongoApiV1ServerDev: {
+		adapter: 'sails-mongo',
+		host: 'cluster0-shard-00-00-nsd2f.mongodb.net',
+		port: 27017,
+		user: 'db_user',
+		password: '9etTRUP3ifFCO2Jc',
+		database: 'aablefishing-api-v1-dev'
+	},
 
 };
